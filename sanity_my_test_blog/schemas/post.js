9149e54b807c2668a839e9657174fc9928ac689f
Alexandr Naum <mfq07k9cz@gmail.com>
2023-01-03 19:43:15 +0300

    schemas post

--- sanity_my_test_blog/schemas/post.js
+++ sanity_my_test_blog/schemas/post.js
@@ -1,3 +1,13 @@
+const textEditorStyles = [
+  {title: 'Paragraph', value: 'normal'},
+  {title: 'Heading 1', value: 'h1'},
+  {title: 'Heading 2', value: 'h2'},
+  {title: 'Heading 3', value: 'h3'},
+  {title: 'Bullet', value: 'bullet'},
+  {title: 'Numbered', value: 'number'},
+  {title: 'Quote', value: 'blockquote'},
+]
+
 export default {
   name: 'post',
   type: 'document',
@@ -52,6 +62,19 @@ export default {
         maxLength: 200, // will be ignored if slugify is set
         slugify: (input) => input.toLowerCase().replace(/\s+/g, '-').slice(0, 200),
       },
+    },
+    {
+      title: 'Description',
+      name: 'description',
+      type: 'text',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'body',
+      title: 'Body content',
+      type: 'array',
+      validation: (Rule) => Rule.required(),
+      of: [{type: 'block', styles: textEditorStyles}, {type: 'image'}],
     },
   ],
 }
