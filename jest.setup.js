13ee918ecc0e0712d89653eefe42d2b2b7164bfc
Alexandr Naum <mfq07k9cz@gmail.com>
2023-01-14 13:06:04 +0300

    added testing snapshots components

---
+++ jest.setup.js
@@ -0,0 +1,5 @@
+import '@testing-library/jest-dom/extend-expect'
+import Enzyme from 'enzyme'
+import Adapter from '@cfaester/enzyme-adapter-react-18'
+
+Enzyme.configure({ adapter: new Adapter() })
